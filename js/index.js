9312cfdd448e240869c51cc946be1732481c2d11
SrijanKarki <srijankarki7@gmail.com>
2024-05-21 16:12:44 +0545

    array file

---
+++ js/index.js
@@ -0,0 +1,25 @@
+greeting = "Namaste";
+console.log(greeting);
+
+/* console.log(greeting + ",", "Ram");
+console.log(greeting + ",", "Shyam");
+console.log(greeting + ",", "Sita");
+console.log(greeting + ",", "Hari");
+console.log(greeting + ",", "Gita"); */
+
+let color = "red";
+// let color = "red"; //Redeclaration of let. Cannot redeclare variable careted using let keyword
+console.log(color);
+
+let size; //declaration
+size = "sm";
+size = "md";
+size = "lg";
+
+console.log(size);
+
+const PI = 3.14;
+console.log(PI);
+
+let count;
+console.log(countd);
