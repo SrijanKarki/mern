0741f2bf789abfb4e1e94e412792c14941247b97
SrijanKarki <srijankarki7@gmail.com>
2024-06-20 17:04:05 +0545

    Counter.jsx increment/decrement using useState special variable in React

--- react-base/src/Counter.jsx
+++ react-base/src/Counter.jsx
@@ -1,8 +1,40 @@
+import React, {useState} from "react" //useState is default function of react
+// useState is named import
+/* 
+    function useState(initialValue) {
+    codes ...
+
+    return [ initialValue, mutatorFunction ]
+    }
+ */
 function Counter() {
+
+    let countValue = 100
+
+    // Any value you pass in setState will be passed to state
+    const [state, setState] = useState(100);//array destructuring
+
+    function increment() {
+        console.log("increment");
+        countValue++;
+        console.log({countValue});
+
+        setState(state + 1)
+    }
+
+    function decrement() {
+        setState(state - 1)
+    }
+
+    //The code below doesn't work with normal variable. To rerender it we need to use special variable useState
+    console.log("rendering");
+
     return <div>
-        <h1>Count: 100</h1>
-        <button>increment</button>
+        <h1>Count: {countValue}</h1>
+        <h1>State: {state}</h1>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>decrement</button>
     </div>
 }
-//Click event
+//Click event or Evenet listener
 export default Counter;
